b63a8dc92b37468de6f578da876423793af76dfc
Brian Wong <brianw7218@gmail.com>
2022-10-18 22:10:07 -0700

    Adjust formatting for a comment and add a new comment on how the GameBoard object functions might be reworked later into private functions

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const GameBoard = (() => {
     const updateGameBoardState = (player, boardPosition) => {
         gameBoardState[boardPosition] = player.getSign();
         // probably will want to call some function to render this update change.
-        // function will be written in a displayController module object.
-        // or maybe that render function part of the displayController object will call this function, idk yet
+            // function will be written in a displayController module object.
+            // or maybe that render function part of the displayController object will call this function, idk yet
     };
     const resetGameBoardState = () => {
         gameBoardState.forEach((boardPosition) => {
@@ -23,6 +23,8 @@ const GameBoard = (() => {
         console.log("test");
         return;
     };
+    // consider making update and reset functions private as player could mess with the game via console
+        // perhaps call them in the getGameBoardState() function, with an if statement determining which, if any, needs to be called
     return {getGameBoardState, updateGameBoardState, resetGameBoardState, checkForWinner, 
         // just for testing purposes, remove later as access is unnecessary
         gameBoardState};
