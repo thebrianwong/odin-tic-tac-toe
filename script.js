df54421e3f48275be6bc5d2a6d7b0749fd7a8118
Brian Wong <brianw7218@gmail.com>
2022-10-18 21:59:15 -0700

    Update part of the GameBoard object to use a new Player method and add a method to return the gameboard array

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const GameBoard = (() => {
         null, null, null,
         null, null, null
     ];
-    const updateGameBoardState = (playerSign, boardPosition) => {
-        gameBoardState[boardPosition] = playerSign;
+    const getGameBoardState = () => {
+        return gameBoardState;
+    };
+    const updateGameBoardState = (player, boardPosition) => {
+        gameBoardState[boardPosition] = player.getSign();
         // probably will want to call some function to render this update change.
         // function will be written in a displayController module object.
         // or maybe that render function part of the displayController object will call this function, idk yet
@@ -20,7 +23,7 @@ const GameBoard = (() => {
         console.log("test");
         return;
     };
-    return {updateGameBoardState, resetGameBoardState, checkForWinner, 
+    return {getGameBoardState, updateGameBoardState, resetGameBoardState, checkForWinner, 
         // just for testing purposes, remove later as access is unnecessary
         gameBoardState};
 })();
